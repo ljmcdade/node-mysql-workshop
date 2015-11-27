7ae9a726486ea06729d97193b21a22c43df72068
ljmcdade <ljmcdade@gmail.com>
2015-11-27 22:05:58 +0000

    revised-exercise-4-joining-up-the-data-pt1.js

--- exercise-4-joining-up-the-data-pt1.js
+++ exercise-4-joining-up-the-data-pt1.js
@@ -7,48 +7,42 @@ var connection = mysql.createConnection({
     database: 'addressbook'
 });
 
-var finalResultArray = [];
-var checkEachObject = {};
-//var arr = [];
-
 connection.query('select Account.id as accountId, Account.email as accountEmail, AddressBook.id as addressBookId, AddressBook.name as addressBookName from Account JOIN AddressBook on Account.id=AddressBook.accountId', function(err, result) {
-        if (err) {
-            console.log('err');
-        }
-        else if (result != undefined) {
-            result.forEach(function(result) {
-
-                //var currentId = result.accountId;
-                var currentAccount = checkEachObject[result.accountId];
-                if (currentAccount === undefined) {
-                    currentAccount = {
-                        accountId: result.accountId,
-                        accountEmail: result.accountEmail,
-                        addressBookName: []
-                    };
-
-                    checkEachObject[result.accountId] = currentAccount;
-                    finalResultArray.push(currentAccount);
-                }
-                currentAccount.addressBookName.push(result.addressBookName);
-
-                // for (var i = 0; i < arr.length; i++) {
-                //     console.log("#" + i + " " + arr[i].accountEmail + "\n" + arr[i].addressBookName.join(", ") + "\n -----------");
-                    
-                // }
-                
-                // console.log(arr);
-                
-                console.log(result.accountId + ": " + result.accountEmail + ": " +  "\n" + result.addressBookName);
-            });
-        }
-
-        //arrangeEmails(rows);
-        connection.end();
-        //console.log(finalResultArray);
+    if (err) { //if there's an error
+        console.log('err');
     }
+    var ACCOUNTS = {};
+    var ADBOOKS = {};
+ 
 
+    result.forEach(function(row) {         
+        var acc = {                              //creating new variable  = object with new keys renaming old keys
+            id: row.accountId,
+            email: row.accountEmail,
+            addbooks: []
+        };
+        var ab = {
+            id: row.addressBookId,
+            name: row.addressBookName
+        };
 
-);
+        ACCOUNTS[acc.id] = acc;             //creating new array with new objects
+        ADBOOKS[ab.id] = ab;
+    });
 
+    var finalResults = [];
+    result.forEach(function(row) {
+            var acc = ACCOUNTS[row.accountId];          //creating another new variable that flags where we got a result -- duplicates
+            var ab = ADBOOKS[row.addressBookId];
 
+            acc.addbooks.push(ab.name);   //pushing field addbooks from acc into addressbook array
+
+            if (finalResults.indexOf(acc) === -1) {     //if there's nothing in finalResults array, push the acc which is the array of addressbooks
+                finalResults.push(acc);
+            }});
+            
+            console.log(finalResults);
+
+});
+
+connection.end();
